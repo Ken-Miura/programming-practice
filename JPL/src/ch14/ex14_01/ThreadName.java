941a1887f03c067076079da2bd62801b867df8e9
Ken-Miura <ken.miura1102@gmail.com>
2015-12-02 00:59:55 +0900

    練習問題追加

---
+++ JPL/src/ch14/ex14_01/ThreadName.java
@@ -0,0 +1,17 @@
+/* Copyright (C) 2015 Ken Miura */
+package ch14.ex14_01;
+
+/**
+ * @author Ken Miura
+ *
+ */
+public final class ThreadName {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(Thread.currentThread().getName());
+	}
+
+}
