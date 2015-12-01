e0d151a6e296322e8549f54f028dd59ae3705c2c
Ken-Miura <ken.miura1102@gmail.com>
2015-12-02 01:01:37 +0900

    練習問題追加

--- JPL/src/ch14/ex14_01/ThreadName.java
+++ JPL/src/ch14/ex14_01/ThreadName.java
@@ -7,9 +7,6 @@ package ch14.ex14_01;
  */
 public final class ThreadName {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		System.out.println(Thread.currentThread().getName());
 	}
