fa1dc205ee24c10e6f1736af2e44f1954d7d52e8
Ken-Miura <ken.miura1102@gmail.com>
2015-12-20 03:35:13 +0900

    スレッドプール完成

--- JPL/src/ch14/ex14_10/ThreadPool.java
+++ JPL/src/ch14/ex14_10/ThreadPool.java
@@ -22,8 +22,88 @@ import java.util.Objects;
  */
 public class ThreadPool {
 
-	private final Object lock = new Object();
+	private static class ThreadInPool extends Thread {
+		private final TaskQueue taskQueue;
+		
+		public ThreadInPool (TaskQueue taskQueue) {
+			assert taskQueue != null;
+			this.taskQueue = taskQueue;
+		}
+		
+		@Override
+		public void run () {
+			while (!taskQueue.cenceled() || taskQueue.getCount() > 0) {
+				Runnable task = null;
+				try {
+					task = taskQueue.take();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+					return;
+				}
+				if (task != null) {
+					task.run();	
+				}
+			}
+		}
+	}
+	
+	private static class TaskQueue {
+		private final Runnable[] tasks;
+		private int head = 0;
+		private int tail = 0;
+		private int count = 0;
+		private volatile boolean canceled = false;
+		
+		public TaskQueue (int queueSize) {
+			assert queueSize >= 1;
+			tasks = new Runnable[queueSize];
+		}
+		
+		public synchronized void put (Runnable task) throws InterruptedException {
+			assert task != null;
+			while (count >= tasks.length && !canceled) {
+				wait();
+			}
+			if (count >= tasks.length) {
+				return;
+			}
+			tasks[tail] = task;
+			tail = (tail+1) % tasks.length;
+			count++;
+			notifyAll();
+		}
+		
+		public synchronized Runnable take () throws InterruptedException {
+			while (count <= 0 && !canceled) {
+				wait();
+			}
+			if (count <= 0) {
+				return null;
+			}
+			Runnable task = tasks[head];
+			head = (head+1) % tasks.length;
+			count--;
+			notifyAll();
+			return task;
+		}
+		
+		public int getCount () {
+			return count;
+		}
+		
+		public synchronized void cancel () {
+			canceled = true;
+			notifyAll();
+		}
+		
+		public boolean cenceled (){
+			return canceled;
+		}
+	}
+	
 	private boolean hasStarted = false;
+	private final TaskQueue taskQueue;
+	private final ThreadInPool[] threads;
 	
     /**
      * Constructs ThreadPool.
@@ -41,6 +121,12 @@ public class ThreadPool {
     	if (numberOfThreads < 1) {
     		throw new IllegalArgumentException("numberOfThreads must be 1 or more.");
     	}
+    	taskQueue = new TaskQueue(queueSize);
+    	threads = new ThreadInPool[numberOfThreads];
+    	for (int i=0; i<numberOfThreads; i++) {
+    		threads[i] = new ThreadInPool(taskQueue);
+    		threads[i].setName("thread " + i);
+    	}
     }
 
     /**
@@ -48,17 +134,14 @@ public class ThreadPool {
      *
      * @throws IllegalStateException if threads has been already started.
      */
-    public void start() {
-       	synchronized (lock) {
+    public synchronized void start() {
        		if (hasStarted) {
        			throw new IllegalStateException("start has been already invoked.");
        		}
-       		
-       		// Do something
-       		
-       		//　あとでfinallyに入れる。
+           	for (int i=0; i<threads.length; i++) {
+           		threads[i].start();
+           	}
        		hasStarted = true;
-       	}
     }   
 
     /**
@@ -66,12 +149,19 @@ public class ThreadPool {
      *
      * @throws IllegalStateException if threads has not been started.
      */
-    public void stop() {
-    	//synchronized (lock) {
-    		if (!hasStarted) {
-   				throw new IllegalStateException("start has not been invoked yet.");
-   			}
-    	//}
+    public synchronized void stop() {
+    	if (!hasStarted) {
+   			throw new IllegalStateException("start has not been invoked yet.");
+   		}
+    	taskQueue.cancel();
+       	for (final ThreadInPool t: threads) {
+       		try {
+				t.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				return;
+			}
+       	}
     }
 
     /**
@@ -89,5 +179,11 @@ public class ThreadPool {
        	if (!hasStarted) {
        		throw new IllegalStateException("start has not been invoked yet.");
        	}
+       	try {
+			taskQueue.put(runnable);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			return;
+		}
     }
 }
