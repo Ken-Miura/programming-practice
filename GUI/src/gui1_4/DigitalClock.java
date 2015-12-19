67743eaa89f68b7030dd8790cf4ec1d78cba5d90
Ken-Miura <ken.miura1102@gmail.com>
2015-12-19 11:42:42 +0900

    キャッチされない例外が発生したらエラーダイアログを出すように修正

--- GUI/src/gui1_4/DigitalClock.java
+++ GUI/src/gui1_4/DigitalClock.java
@@ -2,11 +2,30 @@
 package gui1_4;
 
 import java.awt.EventQueue;
+import java.lang.Thread.UncaughtExceptionHandler;
 
 public final class DigitalClock {
 
 	public static void main(String[] args) {
+		Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+			
+			@Override
+			public void uncaughtException(Thread t, Throwable e) {
+				EventQueue.invokeLater(() -> {
+					ErrorMessageDialog.showErrorMessage("Exception has occured in Thread: " + t.getName() + ". Cause: " + e.toString());
+				});	
+			}
+		});
+		
 		EventQueue.invokeLater(() -> {
+			Thread.currentThread().setUncaughtExceptionHandler(new UncaughtExceptionHandler () {
+
+				@Override
+				public void uncaughtException(Thread t, Throwable e) {
+					ErrorMessageDialog.showErrorMessage("Exception has occured in EDT. Cause: " + e.toString());
+				}
+				
+			});
 			new DigitalClockFrame();
 		});
 	}
