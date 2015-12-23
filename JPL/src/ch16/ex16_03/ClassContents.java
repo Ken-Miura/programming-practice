fd8552200541d555316e21f65c7eb5ee4668cdee
Ken-Miura <ken.miura1102@gmail.com>
2015-12-24 01:23:19 +0900

    練習問題追加

---
+++ JPL/src/ch16/ex16_03/ClassContents.java
@@ -0,0 +1,17 @@
+/* Copyright (C) 2015 Ken Miura */
+package ch16.ex16_03;
+
+/**
+ * @author Ken Miura
+ *
+ */
+public class ClassContents {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
